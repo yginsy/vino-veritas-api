e114ddcced3592a62339b9077ec649d1813e975e
yginsy <yginsy@gmail.com>
2026-05-26 23:32:59 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
+CORS(app)
 
 # loading model
 model = joblib.load("wine_model.pkl")
